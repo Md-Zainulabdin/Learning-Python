fada434b661bdf2e78057126a6d285b186f59f59
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-03-20 06:32:23 +0500

    hello world in python

---
+++ 01Basic/hello_world.py
@@ -0,0 +1,6 @@
+# Hello World in Python
+
+# In  the following code, we will write a simple "Hello World" program using Python.
+# This is one of the simplest programs you can create with Python 
+
+print("Hello, World!") 
