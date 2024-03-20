6491ea0ab4a3401006f241c29bf539166b6472e5
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-03-20 06:34:07 +0500

    Variables in python with their respective data types

---
+++ 02Variables/main.py
@@ -0,0 +1,31 @@
+# Starting Variables and Data Types in Python
+
+#There are three  types of data in python:
+
+# 1. Numeric data types - int, float, complex numbers
+# 2. Non-numeric data types - string, boolean, list, tuple, dictionary
+# 3. Special data types - None, set
+
+## Numeric data types ##
+
+num = 14
+print(type(num))   # Output : <class 'int'>
+
+float_num = 14.02
+print(type(float_num))    # Output : <class 'float'>
+
+complex_num = 5 +  7j   
+print(type(complex_num)) # Output : <class 'complex'>
+
+
+## Non-numeric data types ##
+
+string = 'Hello World'
+print(type(string))     # Output : <class 'str'>
+
+bool_val = True
+print(type(bool_val))     # Output : <class 'bool'>
+
+
+# print( float_num + num) # Check Concatenation in Python
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Welcome to my Python Learning Journey Repository!
+
+<p>
+This repository serves as a comprehensive documentation of my day-to-day progress in learning Python. From foundational concepts to advanced techniques, I'm diligently recording my insights, challenges, and solutions to provide a valuable resource for fellow learners. Whether you're a beginner seeking guidance or an experienced coder looking to refine your skills, this repository aims to offer clear explanations, practical examples, and helpful tips to facilitate your Python learning journey. Join me as I explore the vast world of Python programming and unlock its endless possibilities!
+</p>
