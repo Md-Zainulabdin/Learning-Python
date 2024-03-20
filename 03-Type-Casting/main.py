a9af8d183456157fa7caebc4ee61b66a7e375303
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-03-20 06:47:45 +0500

    Basic type conversion in python

---
+++ 03-Type-Casting/main.py
@@ -0,0 +1,18 @@
+# Data Types: Casting & Conversion
+
+num = 12
+float_num = 12.02
+
+
+# Convert int to float
+
+print(float(num)) # Output : 12.0
+
+# Convert float to int
+
+print(int(float_num)) # Output : 12
+
+# Convert int to str
+
+print(str(num))
+print(type(str(num))) # Output : <class 'str'>
