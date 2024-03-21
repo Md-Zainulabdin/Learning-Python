a2b97abe996890252b9de89d102b2d16e99186ac
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-03-21 07:18:50 +0500

    basic function in python

---
+++ 04-Functions/main.py
@@ -0,0 +1,8 @@
+# Functions: Input & Print
+
+
+#  Input() function prompts the user to input something and returns it as a string.
+name = input('Enter your name :')
+
+# print() function is used to display output on the console or any other output device.
+print('Hello ' + name)  # Concatenating strings using "+" operator
